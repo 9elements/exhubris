b12d0f058b5c9b185d3f5db6a455ab5b5b0e8a09
Marvin Gudel <marvin.gudel@9elements.com>
2025-08-07 12:30:57 +0200

    Port startup code to stm32-metapac (build success, pll enable still hangs)

--- kernel-generic-stm32h753/src/main.rs
+++ kernel-generic-stm32h753/src/main.rs
@@ -3,6 +3,7 @@
 
 mod startup;
 
+use device::rcc::vals::*;
 use stm32_metapac::{
     self as device,
     // flash::vals::Latency,
@@ -14,42 +15,46 @@ use cortex_m_rt::entry;
 #[entry]
 fn main() -> ! {
     const CYCLES_PER_MS: u32 = 400_000;
-    const CLOCKS: ClockConfig = startup::ClockConfig {
+    const CLOCKS: startup::ClockConfig = startup::ClockConfig {
         // The Nucleo board doesn't include an external crystal, so we
         // derive clocks from the HSI64 oscillator.
         source: startup::ClockSource::Hsi64,
         // 64MHz oscillator frequency is outside VCO input range of
         // 2-16, so we use DIVM to divide it by 4 to 16MHz.
-        divm: 4,
+        // divm: 4,
+        divm: Pllm::DIV4,
         // This means the VCO must accept its wider input range:
-        vcosel: device::rcc::vals::Pllvcosel::WIDE_VCO,
-        pllrange: device::rcc::vals::Pllrge::RANGE8,
+        vcosel: Pllvcosel::WIDE_VCO,
+        pllrange: Pllrge::RANGE8,
         // DIVN governs the multiplication of the VCO input frequency to
         // produce the intermediate frequency. We want an IF of 800MHz,
         // or a multiplication of 50x.
         //
         // We subtract 1 to get the DIVN value because the PLL
         // effectively adds one to what we write.
-        divn: 50 - 1,
+        // divn: 50 - 1,
+        divn: Plln::MUL50,
         // P is the divisor from the VCO IF to the system frequency. We
         // want 400MHz, so:
-        divp: device::rcc::vals::Plldiv::DIV2,
+        divp: Plldiv::DIV2,
         // Q produces kernel clocks; we set it to 200MHz:
-        divq: 4 - 1,
+        // divq: 4 - 1,
+        divq: Plldiv::DIV4,
         // R is mostly used by the trace unit and we leave it fast:
-        divr: 2 - 1,
+        // divr: 2 - 1,
+        divr: Plldiv::DIV2,
 
         // We run the CPU at the full core rate of 400MHz:
-        cpu_div: device::rcc::vals::Hpre::DIV1,
+        cpu_div: Hpre::DIV1,
         // We down-shift the AHB by a factor of 2, to 200MHz, to meet
         // its constraints (Table 122 in datasheet)
-        ahb_div: device::rcc::vals::Hpre::DIV2,
+        ahb_div: Hpre::DIV2,
         // We configure all APB for 100MHz. These are relative to the
         // AHB frequency.
-        apb1_div: device::rcc::vals::Ppre::DIV2,
-        apb2_div: device::rcc::vals::Ppre::DIV2,
-        apb3_div: device::rcc::vals::Ppre::DIV2,
-        apb4_div: device::rcc::vals::Ppre::DIV2,
+        apb1_div: Ppre::DIV2,
+        apb2_div: Ppre::DIV2,
+        apb3_div: Ppre::DIV2,
+        apb4_div: Ppre::DIV2,
 
         // Flash runs at 200MHz: 2WS, 2 programming cycles. See
         // reference manual Table 13.
@@ -57,6 +62,6 @@ fn main() -> ! {
         flash_write_delay: 2,
     };
 
-    system_init(CLOCKS);
-    unsafe { kern::startup::start_kernel(CYCLES_PER_MS) }
+    startup::system_init(CLOCKS);
+    unsafe { hubris_kern::startup::start_kernel(CYCLES_PER_MS) }
 }
--- kernel-generic-stm32h753/src/startup.rs
+++ kernel-generic-stm32h753/src/startup.rs
@@ -24,25 +24,34 @@ unsafe fn system_pre_init() {
     // Synthesize a pointer using a const fn (which won't hit RAM) and then
     // convert it to a reference. We can have a reference to PWR because it's
     // hardware, and is thus not uninitialized.
-    let pwr = device::PWR.as_ptr();
+    // let pwr = device::PWR.as_ptr();
     // Poke CR3 to enable the LDO and prevent further writes.
-    pwr.cr3.modify(|_, w| w.ldoen().set_bit());
+    // pwr.cr3().modify(|_, w| w.ldoen().set_bit());
+    device::PWR.cr3().modify(|w| w.set_ldoen(true));
 
     // Busy-wait until the ACTVOSRDY bit says that we've stabilized at VOS3.
-    while !pwr.csr1.read().actvosrdy().bit() {
-        // spin
+    // while !pwr.csr1.read().actvosrdy().bit() {
+    //     // spin
+    // }
+    while !device::PWR.csr1().read().actvosrdy() {
+        //spin
     }
 
     // Turn on the internal RAMs.
-    let rcc = device::RCC.as_ptr();
-    rcc.ahb2enr.modify(|_, w| {
-        w.sram1en()
-            .set_bit()
-            .sram2en()
-            .set_bit()
-            .sram3en()
-            .set_bit()
-    });
+    // let rcc = device::RCC.as_ptr();
+    // rcc.ahb2enr.modify(|_, w| {
+    //     w.sram1en()
+    //         .set_bit()
+    //         .sram2en()
+    //         .set_bit()
+    //         .sram3en()
+    //         .set_bit()
+    // });
+    device::RCC.ahb2enr().modify(|w| {
+        w.set_sram1en(true);
+        w.set_sram2en(true);
+        w.set_sram3en(true);
+    })
 
     // Okay, yay, we can use some RAMs now.
 
@@ -51,13 +60,13 @@ unsafe fn system_pre_init() {
 
 pub struct ClockConfig {
     pub source: ClockSource,
-    pub divm: u8,
+    pub divm: device::rcc::vals::Pllm,
     pub vcosel: device::rcc::vals::Pllvcosel,
     pub pllrange: device::rcc::vals::Pllrge,
-    pub divn: u16,
+    pub divn: device::rcc::vals::Plln,
     pub divp: device::rcc::vals::Plldiv,
-    pub divq: u8,
-    pub divr: u8,
+    pub divq: device::rcc::vals::Plldiv,
+    pub divr: device::rcc::vals::Plldiv,
     pub cpu_div: device::rcc::vals::Hpre,
     pub ahb_div: device::rcc::vals::Hpre,
     pub apb1_div: device::rcc::vals::Ppre,
@@ -73,32 +82,27 @@ pub enum ClockSource {
     Hsi64,
 }
 
-pub fn system_init(config: ClockConfig) -> device::Peripherals {
-    // Use the crate peripheral take mechanism to get peripherals.
+pub fn system_init(config: ClockConfig) {
     let cp = cortex_m::Peripherals::take().unwrap();
-    let p = device::Peripherals::take().unwrap();
 
-    system_init_custom(cp, p, config)
+    system_init_custom(cp, config)
 }
 
-pub fn system_init_custom(
-    mut cp: cortex_m::Peripherals,
-    p: device::Peripherals,
-    config: ClockConfig,
-) -> device::Peripherals {
+pub fn system_init_custom(mut cp: cortex_m::Peripherals, config: ClockConfig) {
     // Basic RAMs are working, power is stable, and the runtime has initialized
     // static variables.
     //
     // We are running at 64MHz on the HSI oscillator at voltage scale VOS3.
 
+    // TODO
     // #[cfg(any(feature = "h743", feature = "h753"))]
-    {
-        // Workaround for erratum 2.2.9 "Reading from AXI SRAM may lead to data
-        // read corruption" - limits AXI SRAM read concurrency.
-        p.AXI
-            .targ7_fn_mod
-            .modify(|_, w| w.read_iss_override().set_bit());
-    }
+    // {
+    //     // Workaround for erratum 2.2.9 "Reading from AXI SRAM may lead to data
+    //     // read corruption" - limits AXI SRAM read concurrency.
+    //     p.AXI
+    //         .targ7_fn_mod
+    //         .modify(|_, w| w.read_iss_override().set_bit());
+    // }
 
     // The H7 -- and perhaps the Cortex-M7 -- has the somewhat annoying
     // property that any attempt to use ITM without having TRCENA set in
@@ -114,36 +118,32 @@ pub fn system_init_custom(
     cp.DCB.enable_trace();
 
     // Make sure debugging works in standby.
-    p.DBGMCU.cr.modify(|_, w| {
-        w.d3dbgcken()
-            .set_bit()
-            .d1dbgcken()
-            .set_bit()
-            .dbgstby_d1()
-            .set_bit()
-            .dbgstop_d1()
-            .set_bit()
-            .dbgsleep_d1()
-            .set_bit()
+    device::DBGMCU.cr().modify(|w| {
+        w.set_d1dbgcken(true);
+        w.set_dbgstby_d1(true);
+        w.set_dbgstop_d1(true);
+        w.set_dbgsleep_d1(true);
     });
+
     // Halt I2C timeout clocks when the debugger halts the system.
-    p.DBGMCU.apb1lfz1.modify(|_, w| {
-        w.dbg_i2c1().set_bit();
-        w.dbg_i2c2().set_bit();
-        w.dbg_i2c3().set_bit();
-        w
+    device::DBGMCU.apb1lfzr1().modify(|w| {
+        w.set_i2c1(true);
+        w.set_i2c2(true);
+        w.set_i2c3(true);
     });
-    p.DBGMCU.apb4fz1.modify(|_, w| {
-        w.dbg_i2c4().set_bit();
-        w
+    device::DBGMCU.apb4fzr1().modify(|w| {
+        w.set_i2c4(true);
     });
 
     // Set up SYSCFG selections so drivers don't have to.
-    p.RCC.apb4enr.modify(|_, w| w.syscfgen().enabled());
+    device::RCC.apb4enr().modify(|w| w.set_syscfgen(true));
     cortex_m::asm::dmb();
 
     // Ethernet is on RMII, not MII.
-    p.SYSCFG.pmcr.modify(|_, w| unsafe { w.epis().bits(0b100) });
+    // p.SYSCFG.pmcr.modify(|_, w| unsafe { w.epis().bits(0b100) });
+    device::SYSCFG
+        .pmcr()
+        .modify(|w| w.set_eth_sel_phy(stm32_metapac::syscfg::vals::EthSelPhy::RMII));
 
     // Turn on CPU I/D caches to improve performance at the higher clock speeds
     // we're about to enable.
@@ -165,9 +165,15 @@ pub fn system_init_custom(
     // We want to set the same bits on both SoCs despite the naming differences.
     // On the H7B3, the register we're calling "D3CR" here is called "SRDCR" in
     // certain editions of the manual.
-    p.PWR.d3cr.write(|w| unsafe { w.vos().bits(0b11) });
+    // p.PWR.d3cr.write(|w| unsafe { w.vos().bits(0b11) });
+    device::PWR
+        .d3cr()
+        .write(|w| w.set_vos(stm32_metapac::pwr::vals::Vos::SCALE1));
     // Busy-wait for the voltage to reach the right level.
-    while !p.PWR.d3cr.read().vosrdy().bit() {
+    // while !p.PWR.d3cr.read().vosrdy().bit() {
+    //     // spin
+    // }
+    while !device::PWR.d3cr().read().vosrdy() {
         // spin
     }
     // We are now at target voltage.
@@ -178,18 +184,22 @@ pub fn system_init_custom(
             // clock source, to get higher accuracy than the internal
             // oscillator. To do that we must turn on the High Speed External
             // oscillator.
-            p.RCC.cr.modify(|_, w| w.hseon().set_bit());
+            device::RCC.cr().modify(|w| w.set_hseon(true));
             // Wait for it to stabilize.
-            while !p.RCC.cr.read().hserdy().bit() {
+            while !device::RCC.cr().read().hserdy() {
                 // spin
             }
 
             // The clock generator divides the external crystal frequency by
             // DIVM before feeding it to the VCO, and the result must be in the
             // range 2-16MHz.
-            p.RCC
-                .pllckselr
-                .modify(|_, w| w.divm1().bits(config.divm).pllsrc().hse());
+            // p.RCC
+            //     .pllckselr
+            //     .modify(|_, w| w.divm1().bits(config.divm).pllsrc().hse());
+            device::RCC.pllckselr().modify(|w| {
+                w.set_divm(1, config.divm);
+                w.set_pllsrc(stm32_metapac::rcc::vals::Pllsrc::HSE);
+            });
 
             // The VCO itself needs to be configured for the appropriate input
             // range and output range. We will also want its P-output, which is
@@ -197,17 +207,12 @@ pub fn system_init_custom(
             //
             // We turn on the Q-output because it's used for a lot of peripheral
             // clocks, and the R-output for the trace unit.
-            p.RCC.pllcfgr.modify(|_, w| {
-                w.pll1vcosel()
-                    .variant(config.vcosel)
-                    .pll1rge()
-                    .variant(config.pllrange)
-                    .divp1en()
-                    .enabled()
-                    .divq1en()
-                    .enabled()
-                    .divr1en()
-                    .enabled()
+            device::RCC.pllcfgr().modify(|w| {
+                w.set_pllvcosel(1, config.vcosel);
+                w.set_pllrge(1, config.pllrange);
+                w.set_divpen(1, true);
+                w.set_divqen(1, true);
+                w.set_divren(1, true);
             });
 
             // Now, we configure the VCO for reals.
@@ -225,87 +230,71 @@ pub fn system_init_custom(
             // We set the R output to the same frequency because it's what
             // Humility currently expects, and drop the Q output for kernel
             // clock use.
-            p.RCC.pll1divr.modify(|_, w| unsafe {
-                w.divn1()
-                    .bits(config.divn)
-                    .divp1()
-                    .variant(config.divp)
-                    .divq1()
-                    .bits(config.divq)
-                    .divr1()
-                    .bits(config.divr)
+            device::RCC.plldivr(1).modify(|w| {
+                w.set_plln(config.divn);
+                w.set_pllp(config.divp);
+                w.set_pllq(config.divq);
+                w.set_pllr(config.divr);
             });
         }
         ClockSource::Hsi64 => {
-            p.RCC
-                .pllckselr
-                .write(|w| w.pllsrc().hsi().divm1().bits(config.divm));
-            p.RCC.pllcfgr.write(|w| {
-                w.pll1vcosel()
-                    .variant(config.vcosel)
-                    .pll1rge()
-                    .variant(config.pllrange)
-                    .divp1en()
-                    .enabled()
-                    .divr1en()
-                    .enabled()
+            device::RCC.pllckselr().write(|w| {
+                w.set_pllsrc(stm32_metapac::rcc::vals::Pllsrc::HSI);
+                w.set_divm(1, config.divm);
             });
-            p.RCC.pll1divr.write(|w| unsafe {
-                w.divp1()
-                    .bits(config.divp as u8)
-                    .divn1()
-                    .bits(config.divn)
-                    .divq1()
-                    .bits(config.divq)
-                    .divr1()
-                    .bits(config.divr)
+            device::RCC.pllcfgr().write(|w| {
+                w.set_pllvcosel(1, config.vcosel);
+                w.set_pllrge(1, config.pllrange);
+                w.set_divpen(1, true);
+                w.set_divren(1, true);
+            });
+            device::RCC.plldivr(1).write(|w| {
+                w.set_pllp(config.divp);
+                w.set_plln(config.divn);
+                w.set_pllq(config.divq);
+                w.set_pllr(config.divr);
             });
         }
     }
 
     // Turn on PLL1 and wait for it to lock.
-    p.RCC.cr.modify(|_, w| w.pll1on().on());
-    while !p.RCC.cr.read().pll1rdy().bit() {
-        // spin
+    device::RCC.cr().modify(|w| w.set_pllon(1, true));
+    while !device::RCC.cr().read().pllrdy(1) {
+        //spin
     }
 
     // PLL1's frequency will become the system clock, which in turn goes through
     // a series of dividers to produce clocks for each system bus.
     // Configure peripheral clock dividers to make sure we stay within
     // range when we change oscillators.
-    p.RCC.d1cfgr.write(|w| {
-        w.d1cpre()
-            .variant(config.cpu_div)
-            .hpre()
-            .variant(config.ahb_div)
-            .d1ppre()
-            .variant(config.apb3_div)
+    device::RCC.d1cfgr().write(|w| {
+        w.set_d1cpre(config.cpu_div);
+        w.set_hpre(config.ahb_div);
+        w.set_d1ppre(config.apb3_div);
     });
+
     // Other APB buses at HCLK/2 = CPU/4 = 100MHz
-    p.RCC.d2cfgr.write(|w| {
-        w.d2ppre1()
-            .variant(config.apb1_div)
-            .d2ppre2()
-            .variant(config.apb2_div)
+    device::RCC.d2cfgr().write(|w| {
+        w.set_d2ppre1(config.apb1_div);
+        w.set_d2ppre2(config.apb2_div);
     });
-    p.RCC.d3cfgr.write(|w| w.d3ppre().variant(config.apb4_div));
+    device::RCC
+        .d3cfgr()
+        .write(|w| w.set_d3ppre(config.apb4_div));
 
     // Flash must be configured with wait states and programming delays to
     // conform to the target speed; see ref man Table 13
-    p.FLASH.acr.write(|w| unsafe {
-        w.latency()
-            .bits(config.flash_latency)
-            .wrhighfreq()
-            .bits(config.flash_write_delay)
+    device::FLASH.acr().write(|w| {
+        w.set_latency(config.flash_latency);
+        w.set_wrhighfreq(config.flash_write_delay);
     });
     loop {
-        let r = p.FLASH.acr.read();
-        if r.latency().bits() == config.flash_latency
-            && r.wrhighfreq().bits() == config.flash_write_delay
-        {
+        let r = device::FLASH.acr().read();
+        if r.latency() == config.flash_latency && r.wrhighfreq() == config.flash_write_delay {
             break;
         }
     }
+
     // Not that reordering is likely here, since we polled, but: we
     // really do need the Flash to be programmed with more wait states
     // before switching the clock.
@@ -313,18 +302,18 @@ pub fn system_init_custom(
 
     // Right! We're all set to change our clock without overclocking anything by
     // accident. Perform the switch.
-    p.RCC.cfgr.write(|w| w.sw().pll1());
-    while !p.RCC.cfgr.read().sws().is_pll1() {
+    device::RCC
+        .cfgr()
+        .write(|w| w.set_sw(stm32_metapac::rcc::vals::Sw::PLL1_P));
+    while device::RCC.cfgr().read().sws() != device::rcc::vals::Sw::PLL1_P {
         // spin
     }
 
     // set RNG clock to PLL1 clock
     // #[cfg(any(feature = "h743", feature = "h753"))]
-    p.RCC.d2ccip2r.modify(|_, w| w.rngsel().pll1_q());
+    device::RCC
+        .d2ccip2r()
+        .modify(|w| w.set_rngsel(stm32_metapac::rcc::vals::Rngsel::PLL1_Q));
 
     // Hello from target speed!
-
-    // Hand the peripherals back in case the board-specific setup code needs to
-    // do anything.
-    p
 }
